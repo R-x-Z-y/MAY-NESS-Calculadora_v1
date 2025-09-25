c9c3e66ce98aa77e417f864312ae9f10d1628e72
ARMENTA MARTINEZ ROBERTO RAZIEL <rraziel14@gmail.com>
2025-09-25 08:46:17 -0600

    Update SUM_FUNCITION.py

--- SUM_FUNCITION.py
+++ SUM_FUNCITION.py
@@ -1,4 +1,4 @@
-print("Funcion de multiplicacion")
+print("Funcion de suma")
 n1 = float(input("Ingresa n1: "))
 n2 = float((input("Ingresa n2: ")))
-print("El resultado de multiplicar los 2 numeros es: ", n1 * n2 )
+print("El resultado de multiplicar los 2 numeros es: ", n1 + n2 )
